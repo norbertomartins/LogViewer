94bf639b93f380d4d3d8f064686906b7ad09785e
norbertomartins <norberto.martins@gmail.com>
2026-08-05 02:37:59 +0100

    Fix horizontal scroll bug

--- src/LogViewer.App/Views/Documents/TailDocumentView.xaml.cs
+++ src/LogViewer.App/Views/Documents/TailDocumentView.xaml.cs
@@ -1,5 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
 using LogViewer.App.Models;
 using LogViewer.App.ViewModels;
 
@@ -8,6 +10,7 @@ namespace LogViewer.App.Views.Documents;
 public partial class TailDocumentView : UserControl
 {
     private TailDocumentViewModel? _viewModel;
+    private ScrollViewer? _lineListScrollViewer;
 
     public TailDocumentView()
     {
@@ -40,11 +43,55 @@ public partial class TailDocumentView : UserControl
             {
                 LineListView.ScrollIntoView(LineListView.Items[^1]);
             }
+
+            ResetHorizontalScrollAfterLayout();
         });
     }
 
     private void OnScrollToLineRequested(LogLineViewModel line)
     {
-        Dispatcher.BeginInvoke(() => LineListView.ScrollIntoView(line));
+        Dispatcher.BeginInvoke(() =>
+        {
+            LineListView.ScrollIntoView(line);
+            ResetHorizontalScrollAfterLayout();
+        });
+    }
+
+    /// <summary>ScrollIntoView on a long (unwrapped) line can drag the horizontal scroll offset away from
+    /// zero, hiding the start of every line. Line starts matter far more than the tail of one long line, so
+    /// pin it back to zero once the ScrollIntoView layout pass has actually run.</summary>
+    private void ResetHorizontalScrollAfterLayout()
+    {
+        Dispatcher.BeginInvoke(() => GetLineListScrollViewer()?.ScrollToHorizontalOffset(0), DispatcherPriority.ContextIdle);
+    }
+
+    private ScrollViewer? GetLineListScrollViewer()
+    {
+        if (_lineListScrollViewer is null && LineListView.IsLoaded)
+        {
+            _lineListScrollViewer = FindDescendant<ScrollViewer>(LineListView);
+        }
+
+        return _lineListScrollViewer;
+    }
+
+    private static T? FindDescendant<T>(DependencyObject root) where T : DependencyObject
+    {
+        var childCount = VisualTreeHelper.GetChildrenCount(root);
+        for (var i = 0; i < childCount; i++)
+        {
+            var child = VisualTreeHelper.GetChild(root, i);
+            if (child is T match)
+            {
+                return match;
+            }
+
+            if (FindDescendant<T>(child) is { } descendant)
+            {
+                return descendant;
+            }
+        }
+
+        return null;
     }
 }
